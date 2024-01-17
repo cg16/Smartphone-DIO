8fbe7a41b03b3dcc6118f813721ef8e59c281583
Celso Guilherme <78118860+cg16@users.noreply.github.com>
2024-01-17 19:53:19 -0300

    Update Program.cs

--- SM DIO/Program.cs
+++ SM DIO/Program.cs
@@ -8,6 +8,6 @@ nokia.InstalarAplicativo("Whatsapp");
 Console.WriteLine("\n");
 
 Console.WriteLine("Smartphone iPhone:");
-Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "222222222", memoria: 128);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
